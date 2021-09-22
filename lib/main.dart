a8e12b4e707c2e6fefea2cf2c1ae71197c9dfd66
albertrein <guilhermealberto8@gmail.com>
2021-09-22 22:25:55 +0000

    componentizando menu do rodape.

--- lib/main.dart
+++ lib/main.dart
@@ -101,72 +101,7 @@ class _MyHomePageState extends State<MyHomePage> {
           SalaVirtual("Sociedade e Contemporaneidade",'2','2','5', Colors.green),
         ],
       ),
-      bottomNavigationBar: BottomNavigationBar(
-        items: [
-          BottomNavigationBarItem(
-            icon: Stack(
-              children: <Widget>[
-                new Icon(Icons.message_outlined),
-                new Positioned(
-                  right: 0,
-                  child: new Container(
-                    padding: EdgeInsets.all(1),
-                    decoration: new BoxDecoration(
-                      color: Colors.red,
-                      borderRadius: BorderRadius.circular(6),
-                    ),
-                    constraints: BoxConstraints(
-                      minWidth: 12,
-                      minHeight: 12,
-                    ),
-                    child: new Text(
-                      '0',
-                      style: new TextStyle(
-                        color: Colors.white,
-                        fontSize: 8,
-                      ),
-                      textAlign: TextAlign.center,
-                    ),
-                  ),
-                )
-              ],
-            ),
-            label: ('Mensagens'),
-          ),
-          BottomNavigationBarItem(
-            icon: new Stack(
-              children: <Widget>[
-                new Icon(Icons.add_alert_outlined),
-                new Positioned(
-                  right: 0,
-                  child: new Container(
-                    padding: EdgeInsets.all(1),
-                    decoration: new BoxDecoration(
-                      color: Colors.red,
-                      borderRadius: BorderRadius.circular(6),
-                    ),
-                    constraints: BoxConstraints(
-                      minWidth: 12,
-                      minHeight: 12,
-                    ),
-                    child: new Text(
-                      '12',
-                      style: new TextStyle(
-                        color: Colors.white,
-                        fontSize: 8,
-                      ),
-                      textAlign: TextAlign.center,
-                    ),
-                  ),
-                )
-              ],
-            ),
-            label: ('Avisos'),
-          ),
-        ],
-        backgroundColor: Colors.black54,
-        selectedItemColor: Colors.yellow,
-      )
+      bottomNavigationBar: MenuRodape()
     );
   }
 }
--- lib/menu_rodape.dart
+++ lib/menu_rodape.dart
@@ -3,9 +3,71 @@ import 'package:flutter/material.dart';
 class MenuRodape extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text('qwe'),
-    );
+    return  BottomNavigationBar(
+        items: [
+          BottomNavigationBarItem(
+            icon: Stack(
+              children: <Widget>[
+                new Icon(Icons.message_outlined),
+                new Positioned(
+                  right: 0,
+                  child: new Container(
+                    padding: EdgeInsets.all(1),
+                    decoration: new BoxDecoration(
+                      color: Colors.red,
+                      borderRadius: BorderRadius.circular(6),
+                    ),
+                    constraints: BoxConstraints(
+                      minWidth: 12,
+                      minHeight: 12,
+                    ),
+                    child: new Text(
+                      '0',
+                      style: new TextStyle(
+                        color: Colors.white,
+                        fontSize: 8,
+                      ),
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
+                )
+              ],
+            ),
+            label: ('Mensagens'),
+          ),
+          BottomNavigationBarItem(
+            icon: new Stack(
+              children: <Widget>[
+                new Icon(Icons.add_alert_outlined),
+                new Positioned(
+                  right: 0,
+                  child: new Container(
+                    padding: EdgeInsets.all(1),
+                    decoration: new BoxDecoration(
+                      color: Colors.red,
+                      borderRadius: BorderRadius.circular(6),
+                    ),
+                    constraints: BoxConstraints(
+                      minWidth: 12,
+                      minHeight: 12,
+                    ),
+                    child: new Text(
+                      '6',
+                      style: new TextStyle(
+                        color: Colors.white,
+                        fontSize: 8,
+                      ),
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
+                )
+              ],
+            ),
+            label: ('Avisos'),
+          ),
+        ],
+        backgroundColor: Colors.black54,
+        selectedItemColor: Colors.yellow,
+      );    
   }
-
 }
